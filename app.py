7aeb53889b343cf1354e51b6653b543c1da21138
gwilson253 <gwilson253@gmail.com>
2018-05-13 17:17:47 -0700

    started line_graph.js

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 
 from flask import Flask, render_template
+from math import sin
+import json
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -19,6 +20,13 @@ def bar_graph():
 @app.route('/choropleth_graph')
 def choropleth_graph():
     return render_template('choropleth_graph.html')
+
+@app.route('/data/line_graph')
+def data_line_graph():
+    data = {}
+    data['x'] = [_ for _ in range(-10, 11)]
+    data['y'] = [sin(_) * 100 for _ in data['x']]
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
